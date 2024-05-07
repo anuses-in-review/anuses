31c9c2355148c7c724ac1ee9c586364f7ce3f2e1
Your Mom <chisme.loteria@gmail.com>
2024-05-06 20:23:59 -0500

    some pics of women but mostly pics of hamburgers

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-const dev = process.argv.includes('dev')
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
+
+const dev = process.argv.includes('dev');
+
+export default {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            // Optionally specify additional options for the adapter
+            // For example, you can specify the output directory
+            // output: 'build' // Output directory for the static files (default is 'build')
+        }),
+
         paths: {
+            // Use BASE_PATH environment variable in production
             base: dev ? '' : process.env.BASE_PATH,
         }
     }
 };
-
-export default config;
